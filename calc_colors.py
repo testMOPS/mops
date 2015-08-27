9eb5e6756ed555746981b15cadee95ed6f21ff37
Lynne Jones <lynnej@uw.edu>
2015-08-27 13:17:51 -0700

    Added code to calculate LSST colors for asteroid spectra types.

---
+++ calc_colors.py
@@ -0,0 +1,52 @@
+# Requirements: LSST packages photUtils and throughputs
+# Get the asteroid spectra and harris_V throughput curve at http://www.astro.washington.edu/users/lynnej/seds/asteroids.tar
+# Run this in the directory containing the asteroids SEDs. (note that it assumes there are no extraneous *.dat files in that directory).
+
+import os
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+from lsst.sims.photUtils import Bandpass, Sed
+
+filterdir = os.getenv('LSST_THROUGHPUTS_BASELINE')
+filterlist = ('u', 'g', 'r', 'i', 'z', 'y')
+
+lsst = {}
+for f  in filterlist:
+    lsst[f] = Bandpass()
+    lsst[f].readThroughput(os.path.join(filterdir, 'total_' + f + '.dat'))
+harrisV = Bandpass()
+harrisV.readThroughput('harris_V.dat')
+
+seds = glob.glob('*.dat')
+seds.remove('harris_V.dat')
+
+print 'Calculating colors for seds: \n', seds
+
+writestring = 'Sed '
+for f in filterlist:
+    writestring += 'V-%s ' %(f)
+print writestring
+
+
+fig = plt.figure()
+for s in seds:
+    sed = Sed()
+    sed.readSED_flambda(s)
+    sname = s.replace('.dat', '')
+    vmag = sed.calcMag(harrisV)
+    colors = {}
+    writestring = '%s ' %(sname)
+    for f in filterlist:
+        mag = sed.calcMag(lsst[f])
+        colors[f] = vmag - mag
+        writestring += '%.3f ' %(colors[f])
+    print writestring
+    plt.plot(colors['g'], colors['r'], linestyle='', marker='o', label='%s' %(sname))
+
+plt.xlabel('V-g')
+plt.ylabel('V-r')
+plt.legend(fancybox=True, numpoints=1, fontsize='smaller')
+plt.show()
+
+
