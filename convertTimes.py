4df64ec9522f7e7febf1d501f6ee7bec283bac4e
Lynne Jones <lynnej@uw.edu>
2015-10-23 17:24:40 -0700

    Updated comments.

--- convertTimes.py
+++ convertTimes.py
@@ -8,7 +8,7 @@ from astropy.time import Time
 
 if len(sys.argv) < 4:
     print "usage: python convertTimes.py YourTime YourFormat YourTimeScale"
-    print " YourFormat ought to be in JD or MJD (but could be ISO as well)."
+    print " YourFormat ought to be in JD or MJD"
     print " outputs: your time in many other timescales"
     exit()
 
@@ -21,10 +21,11 @@ print "Converting %f / %s / %s to other timescales." %(yourtime, yourformat, you
 
 t = Time(yourtime, format=yourformat, scale=yourtimescale)
 
+print 'UTC: %s' %(t.utc.iso)
+print 'UTC: %f %f' %(t.utc.jd, t.utc.mjd)
 print 'TT: %f %f' %(t.tt.jd, t.tt.mjd)
 print 'TAI: %f %f' %(t.tai.jd, t.tai.mjd)
 print 'TDB: %f %f' %(t.tdb.jd, t.tdb.mjd)
-print 'UTC: %f %f' %(t.utc.jd, t.utc.mjd)
 #print 'UT1: %f %f' %(t.ut1.jd, t.ut1.mjd)
 
 
